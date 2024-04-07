91cc29637b61406acf7b37768defd662e8f43d6c
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-04-08 02:46:29 +0300

    Initial commit

---
+++ src/main/java/com/example/teampsychologist/TeampsychologistApplication.java
@@ -0,0 +1,13 @@
+package com.example.teampsychologist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeampsychologistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeampsychologistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/teampsychologist/TeampsychologistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teampsychologist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeampsychologistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
