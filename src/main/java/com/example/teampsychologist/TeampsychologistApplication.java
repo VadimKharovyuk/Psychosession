d823f3445b1e9f12da7099bb29673a393549708c
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-04-08 02:46:56 +0300

    footer color

--- src/main/java/com/example/teampsychologist/TeampsychologistApplication.java
+++ src/main/java/com/example/teampsychologist/TeampsychologistApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TeampsychologistApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TeampsychologistApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TeampsychologistApplication.class, args);
+    }
 
 }
